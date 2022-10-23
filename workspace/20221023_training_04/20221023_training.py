b2c6a8029b2d61e36eb67842c6f2b6a6e00cf441
maxvo <hananotamashii@gmail.com>
2022-10-23 12:54:11 +0200

    dodanie notatek do nauki, dzień 4

--- workspace/20221023_training_04/20221023_training.py
+++ workspace/20221023_training_04/20221023_training.py
@@ -1,27 +1,27 @@
 ###positional arguments
-def sum(a, b, c):
-    pass
+# def sum(a, b, c):
+#     pass
 
 
 ######     keyword arguments
-def increment(value, step):
-    def increment(value: int, step: int = 1) -> int:  # kolejność 1# keyword 2# pozycyjne
-        """
-        Przykład użycia, w parach
-        >>> test
-        wynik testu
-
-        :param value:
-        :param step:
-        :return:
-        """
-        return value + step
-
-
-print(increment(5, 2))  # Outcome: 7    # posługiwanie się pozycją argumentu
-print(increment(5))  # Outcome: 6
-print(increment(value=5, step=2))  # Outcome: 7
-print(increment(step=5, value=1))  # Outcome: 6
+# def increment(value, step):
+#     def increment(value: int, step: int = 1) -> int:  # kolejność 1# keyword 2# pozycyjne
+#         """
+#         Przykład użycia, w parach
+#         >>> test
+#         wynik testu
+#
+#         :param value:
+#         :param step:
+#         :return:
+#         """
+#         return value + step
+#
+#
+# print(increment(5, 2))  # Outcome: 7    # posługiwanie się pozycją argumentu
+# print(increment(5))  # Outcome: 6
+# print(increment(value=5, step=2))  # Outcome: 7
+# print(increment(step=5, value=1))  # Outcome: 6
 
 #############################################################################
 #############################################################################
@@ -42,3 +42,77 @@ print(increment(step=5, value=1))  # Outcome: 6
 
 #############################################################################
 #############################################################################
+
+"""
+Bez posługiwania się wbudowanymi funkcjami takimi jak max czy min
+napisz funkcję (a może funkcje) która zwróci max z trzech liczb
+"""
+
+
+##>>>>>>>>>>>>>>>>>>>>>   TBD
+
+# from typing import Tuple
+#
+#
+# def wprow_danych(a: int, b: int, c: int) -> Tuple[str, str, str]:
+#     liczba_1 = int(input('Wartość pierwsza:  '))
+#     liczba_2 = int(input('Wartość druga:  '))
+#     liczba_3 = int(input('Wartość trzecia:  '))
+#     return liczba_1, liczba_2, liczba_3
+#
+#
+# wprow_danych(1, 1, 1)
+#
+#
+# def dzialania():
+#     if wprow_danych(liczba1) > liczba2:
+
+
+#############################################################################
+#############################################################################
+
+def max_of_three2(a: float, b: float, c: float) -> float:
+    if (a > b) and (a > c):
+        wynik = a
+    elif (b > a) and (b > c):
+        wynik = b
+    else:
+        wynik = c
+    return wynik
+
+#############################################################################
+#############################################################################
+#############################################################################
+
+
+def max_of_two(a, b):
+    if a >b:
+        return a
+    return b
+
+def max_of_three(a, b, c):
+    return max_of_two(a,max_of_two(b, c))
+
+if __name__ == "__main__":
+    assert max_of_three(1, 3, 2) == 3
+    assert max_of_three(2, 6, -5) == 6
+
+#############################################################################
+#############################################################################
+#############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
