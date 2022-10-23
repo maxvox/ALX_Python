5df5257e0ee5e06ec20782e49faece000f91a8d3
maxvo <hananotamashii@gmail.com>
2022-10-23 10:56:47 +0200

    dodanie notatek do nauki, dzień 4

--- workspace/20221023_training_04/20221023_training.py
+++ workspace/20221023_training_04/20221023_training.py
@@ -1,36 +1,34 @@
-# positional arguments
-# def sum(a, b, c)
+###positional arguments
+def sum(a, b, c):
 
 
-# keyword arguments
-# def increment(value, step))
+# ### ##keyword arguments
+def increment(value, step):
+    def increment(value: int, step: int = 1) -> int:  # kolejność 1# keyword 2# pozycyjne
+        """
+        Przykład użycia, w parach
+        >>> test
+        wynik testu
+
+        :param value:
+        :param step:
+        :return:
+        """
+        return value + step
 
 
-def increment(value: int, step: int = 1) -> int: # kolejność 1# keyword 2# pozycyjne
-    """
-    Przykład użycia, w parach
-    >>> test
-    wynik testu
+print(increment(5, 2))  # Outcome: 7    # posługiwanie się pozycją argumentu
+print(increment(5))  # Outcome: 6
+print(increment(value=5, step=2))  # Outcome: 7
+print(increment(step=5, value=1))  # Outcome: 6
 
-    :param value:
-    :param step:
-    :return:
-    """
-    return value + step
+#############################################################################
+#############################################################################
 
-
-print(increment(5, 2))  #  Outcome: 7    # posługiwanie się pozycją argumentu
-print(increment(5))  #  Outcome: 6
-print(increment(value=5, step=2))  #  Outcome: 7
-print(increment(step=5, value=1))  #  Outcome: 6
-
-##############################################################################
-##############################################################################
-
-### -Napisz testy  -> doctest-
+#### -Napisz testy  -> doctest-
 
 #  postać:   """Docstring"""
-### Notatka
+# ## Notatka
 # utwórz nazwę funkcji 'sygnatura funkcji' > enter > zacznij pisać docstring """ (pierwsze trzy cudzysłowy),
 # samo się uzupełni do sześciu, stojąc pomiędzy 3 a 3 naciśnij enter -> samo się wszystko uzupełni na postać:
 # """
@@ -39,7 +37,7 @@ print(increment(step=5, value=1))  #  Outcome: 6
 # :param step:
 # :return:
 # """
-##############################################################################
-##############################################################################
 
 
+#############################################################################
+#############################################################################
