4f49e7d6bd8893fd80ac0093dffee24013b7726b
maxvo <hananotamashii@gmail.com>
2022-11-07 03:11:48 +0100

    dodanie notatek do nauki, dzień 5

--- git_update.sh
+++ git_update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git add .
-git commit -am "dodanie notatek do nauki, dzień 4"
+git commit -am "dodanie notatek do nauki, dzień 5"
 git checkout master
 git push origin master
 
--- workspace/ALX_20221023_training_04_homework/test_sum_of_digits.py
+++ workspace/ALX_20221023_training_04_homework/test_sum_of_digits.py
@@ -1,10 +1,16 @@
 from ALX_20221023_training_04_homework import sum_of_digits, tuple_01, list_01, set_01, dictionary_01
 
+
 def test_sum_of_digits():
+    # Checking if the sum of the digits in list_01 is equal to 19023.
     assert sum_of_digits(list_01) == 19023
+    # Checking if the sum of the digits in tuple_01 is equal to 7900.
     assert sum_of_digits(tuple_01) == 7900
+    # Checking if the sum of the digits in set_01 is equal to 876663.
     assert sum_of_digits(set_01) == 876663
+    # Checking if the sum of the digits in dictionary_01 is equal to 2993.
     assert sum_of_digits(dictionary_01) == 2993
+    assert sum_of_digits((1, 2, 3)) == 6
 
-
-
+# def test_sum_sum_of_digits():
+#
