35bafcc9e7f19c8d6d47bd64623d86723703d287
maxvo <hananotamashii@gmail.com>
2022-11-08 20:23:36 +0100

    dodanie notatek do nauki, dzień 5

---
+++ workspace/inne/takie_tam.py
@@ -0,0 +1,14 @@
+# x = y = 1
+#
+# # nazwa zmiennej tylko z małych liter
+# nazwa_zmiennej = 1
+#
+#
+# # if jakiswarunek:
+# if x > y:
+#     # zrob costam
+#     pass
+
+x = 0
+if x:
+    print('xx')
